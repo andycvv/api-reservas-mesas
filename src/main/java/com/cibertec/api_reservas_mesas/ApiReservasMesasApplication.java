048ffd237c9b1d45c9cb8bf9638d08b0a69402b0
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-05-17 20:25:10 -0500

    start: crear proyecto

---
+++ src/main/java/com/cibertec/api_reservas_mesas/ApiReservasMesasApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.api_reservas_mesas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiReservasMesasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiReservasMesasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cibertec/api_reservas_mesas/ApiReservasMesasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.api_reservas_mesas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiReservasMesasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
