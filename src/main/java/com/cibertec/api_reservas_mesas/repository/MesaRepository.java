8254365ec7ec123e6579b72c1eff1d8caebb6b71
Yadira Cceline Auccasi Loa <yadira.auccasiloa@gmail.com>
2025-05-17 22:50:55 -0500

    agregar MesaRepository

---
+++ src/main/java/com/cibertec/api_reservas_mesas/repository/MesaRepository.java
@@ -0,0 +1,11 @@
+package com.cibertec.api_reservas_mesas.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cibertec.api_reservas_mesas.model.Mesa;
+
+@Repository
+public interface MesaRepository extends JpaRepository<Mesa, Integer>{
+
+}
