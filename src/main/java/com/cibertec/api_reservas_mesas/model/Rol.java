1108a21b6d24e590047f6a8a834ce43b645d164a
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-05-17 21:17:45 -0500

    agregar entidad Usuario

---
+++ src/main/java/com/cibertec/api_reservas_mesas/model/Rol.java
@@ -0,0 +1,6 @@
+package com.cibertec.api_reservas_mesas.model;
+
+public enum Rol {
+	CLIENTE,
+	RECEPCIONISTA
+}
---
+++ src/main/java/com/cibertec/api_reservas_mesas/model/Usuario.java
@@ -0,0 +1,96 @@
+package com.cibertec.api_reservas_mesas.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nombre;
+	private String clave;
+	private String apellidoPaterno;
+	private String apellidoMaterno;
+	private String telefono;
+	private String dni;
+	private boolean estado;
+	@Enumerated(EnumType.STRING)
+	private Rol rol;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getClave() {
+		return clave;
+	}
+
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+
+	public String getApellidoPaterno() {
+		return apellidoPaterno;
+	}
+
+	public void setApellidoPaterno(String apellidoPaterno) {
+		this.apellidoPaterno = apellidoPaterno;
+	}
+
+	public String getApellidoMaterno() {
+		return apellidoMaterno;
+	}
+
+	public void setApellidoMaterno(String apellidoMaterno) {
+		this.apellidoMaterno = apellidoMaterno;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public boolean isEstado() {
+		return estado;
+	}
+
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+
+	public Rol getRol() {
+		return rol;
+	}
+
+	public void setRol(Rol rol) {
+		this.rol = rol;
+	}
+}
