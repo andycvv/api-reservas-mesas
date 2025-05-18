3078963c69680a3e22d463ee788070877d40af01
Yadira Cceline Auccasi Loa <yadira.auccasiloa@gmail.com>
2025-05-17 22:37:33 -0500

    agregar entidad Mesa

---
+++ src/main/java/com/cibertec/api_reservas_mesas/model/Mesa.java
@@ -0,0 +1,73 @@
+package com.cibertec.api_reservas_mesas.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Mesa {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nombre;
+	private int capacidad;
+	
+	@ManyToOne
+	@JoinColumn(name = "ubicacion_id")
+	private Ubicacion ubicacion;
+	
+	@OneToMany(mappedBy = "mesa")
+	@JsonIgnore
+	private List<Reserva> reservas;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getCapacidad() {
+		return capacidad;
+	}
+
+	public void setCapacidad(int capacidad) {
+		this.capacidad = capacidad;
+	}
+
+	public Ubicacion getUbicacion() {
+		return ubicacion;
+	}
+
+	public void setUbicacion(Ubicacion ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+
+	public List<Reserva> getReservas() {
+		return reservas;
+	}
+
+	public void setReservas(List<Reserva> reservas) {
+		this.reservas = reservas;
+	}
+	
+	
+}
