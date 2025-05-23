71e50588b2855e38632dddc725ada1f60345679d
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-05-23 15:17:55 -0500

    agregar dtos para Ubicacion

---
+++ src/main/java/com/cibertec/api_reservas_mesas/dto/UbicacionCreacionDTO.java
@@ -0,0 +1,13 @@
+package com.cibertec.api_reservas_mesas.dto;
+
+public class UbicacionCreacionDTO {
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
---
+++ src/main/java/com/cibertec/api_reservas_mesas/dto/UbicacionDTO.java
@@ -0,0 +1,25 @@
+package com.cibertec.api_reservas_mesas.dto;
+
+public class UbicacionDTO {
+	private Integer id;
+	private String nombre;
+	private boolean estado;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public boolean isEstado() {
+		return estado;
+	}
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+}
