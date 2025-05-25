91eed06d5b2cf7d5925e3cba3445ca48159c103c
Yadira Cceline Auccasi Loa <yadira.auccasiloa@gmail.com>
2025-05-24 22:21:31 -0500

    validar campos de MesaEdicionDTO

--- src/main/java/com/cibertec/api_reservas_mesas/dto/MesaEdicionDTO.java
+++ src/main/java/com/cibertec/api_reservas_mesas/dto/MesaEdicionDTO.java
@@ -1,11 +1,20 @@
 package com.cibertec.api_reservas_mesas.dto;
 
-public class MesaEdicionDTO {
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
-	private int numero;
-	private int capacidad;
-	private int ubicacionId;
-	private boolean estado;
+public class MesaEdicionDTO {
+	@NotNull(message = "El campo número es obligatorio")
+	@Min(value = 1, message = "El campo número debe ser mayor o igual a 1")
+	private Integer numero;
+	@NotNull(message = "El campo capacidad es obligatorio")
+	@Min(value = 1, message = "El campo capacidad debe ser mayor o igual a 1")
+	private Integer capacidad;
+	@NotNull(message = "El campo ubicacionId es obligatorio")
+	@Min(value = 1, message = "El campo ubicacionId debe ser mayor o igual a 1")
+	private Integer ubicacionId;
+	@NotNull(message = "El campo estado es obligatorio")
+	private Boolean estado;
 	
 	public int getNumero() {
 		return numero;
