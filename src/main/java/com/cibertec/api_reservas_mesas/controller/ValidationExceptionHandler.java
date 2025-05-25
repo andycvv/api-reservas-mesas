09071c7a8bc6f75eb9e27dc536f0bfa54eacfd1b
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-05-24 21:30:58 -0500

    agregar ValidationExceptionHandler

---
+++ src/main/java/com/cibertec/api_reservas_mesas/controller/ValidationExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.cibertec.api_reservas_mesas.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ValidationExceptionHandler {
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> manejarErrores(MethodArgumentNotValidException ex) {
+        Map<String, String> errores = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errores.put(error.getField(), error.getDefaultMessage());
+        });
+        return new ResponseEntity<>(errores, HttpStatus.BAD_REQUEST);
+    }
+}
