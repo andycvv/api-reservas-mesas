c8a8e001506f3cd2eb9efcb408a31cc724803642
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-05-24 21:43:25 -0500

    validar campos de UbicacionCreacionDTO

--- src/main/java/com/cibertec/api_reservas_mesas/dto/UbicacionCreacionDTO.java
+++ src/main/java/com/cibertec/api_reservas_mesas/dto/UbicacionCreacionDTO.java
@@ -1,6 +1,11 @@
 package com.cibertec.api_reservas_mesas.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class UbicacionCreacionDTO {
+	@NotNull(message = "El campo nombre es obligatorio")
+	@Size(min = 3, message = "El campo nombre debe tener como mínimo 3 caracteres")
 	private String nombre;
 
 	public String getNombre() {
