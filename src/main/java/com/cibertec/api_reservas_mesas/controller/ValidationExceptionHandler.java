3c89376f7746ab68eaf59baf75a5da4f207bcc9d
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-05-24 22:43:53 -0500

    agregar metodo handleRangoHorarioInvalido para error personalizado

--- src/main/java/com/cibertec/api_reservas_mesas/controller/ValidationExceptionHandler.java
+++ src/main/java/com/cibertec/api_reservas_mesas/controller/ValidationExceptionHandler.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.cibertec.api_reservas_mesas.exception.RangoHorarioInvalidoException;
+
 @RestControllerAdvice
 public class ValidationExceptionHandler {
 	@ExceptionHandler(MethodArgumentNotValidException.class)
@@ -19,4 +21,11 @@ public class ValidationExceptionHandler {
         });
         return new ResponseEntity<>(errores, HttpStatus.BAD_REQUEST);
     }
+	
+	@ExceptionHandler(RangoHorarioInvalidoException.class)
+	public ResponseEntity<Map<String, String>> handleRangoHorarioInvalido(RangoHorarioInvalidoException ex) {
+        Map<String, String> errores = new HashMap<>();
+        errores.put(ex.getCampo(), ex.getMessage());
+        return new ResponseEntity<>(errores, HttpStatus.BAD_REQUEST);
+	}
 }
