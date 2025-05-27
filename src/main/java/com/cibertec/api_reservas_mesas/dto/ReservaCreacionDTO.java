1320d4685138196892b8bacd5bdf82c05882cd3d
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-05-27 16:11:19 -0500

    agregar dto para la creacion de reservas

---
+++ src/main/java/com/cibertec/api_reservas_mesas/dto/ReservaCreacionDTO.java
@@ -0,0 +1,44 @@
+package com.cibertec.api_reservas_mesas.dto;
+
+import java.time.LocalDate;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public class ReservaCreacionDTO {
+	@NotNull(message = "El campo fecha es obligatorio")
+	private LocalDate fecha;
+	@NotNull(message = "El campo clienteId es obligatorio")
+	@Min(value = 1, message = "El campo clienteId debe ser mayor o igual a 1")
+	private Integer clienteId;
+	@NotNull(message = "El campo mesaId es obligatorio")
+	@Min(value = 1, message = "El campo mesaId debe ser mayor o igual a 1")
+	private Integer mesaId;
+	@NotNull(message = "El campo horarioId es obligatorio")
+	@Min(value = 1, message = "El campo horarioId debe ser mayor o igual a 1")
+	private Integer horarioId;
+	public LocalDate getFecha() {
+		return fecha;
+	}
+	public void setFecha(LocalDate fecha) {
+		this.fecha = fecha;
+	}
+	public Integer getClienteId() {
+		return clienteId;
+	}
+	public void setClienteId(Integer clienteId) {
+		this.clienteId = clienteId;
+	}
+	public Integer getMesaId() {
+		return mesaId;
+	}
+	public void setMesaId(Integer mesaId) {
+		this.mesaId = mesaId;
+	}
+	public Integer getHorarioId() {
+		return horarioId;
+	}
+	public void setHorarioId(Integer horarioId) {
+		this.horarioId = horarioId;
+	}
+}
