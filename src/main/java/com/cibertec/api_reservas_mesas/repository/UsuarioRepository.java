fc97174e30e195535778d723c3d40efe942ed3a0
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-06-14 19:42:24 -0500

    agregar metodo para obtener usuario por DNI

--- src/main/java/com/cibertec/api_reservas_mesas/repository/UsuarioRepository.java
+++ src/main/java/com/cibertec/api_reservas_mesas/repository/UsuarioRepository.java
@@ -1,11 +1,15 @@
 package com.cibertec.api_reservas_mesas.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.cibertec.api_reservas_mesas.model.Usuario;
+import java.util.List;
+
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
-
+	Optional<Usuario> findByDni(String dni);
 }
