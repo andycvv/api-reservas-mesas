bf242fe809da6b6ba78069717fff5c53cd82b4f4
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-06-14 19:42:01 -0500

    modificar relacion de Usuario al crear una entidad Rol con relacion many-to-many

--- src/main/java/com/cibertec/api_reservas_mesas/model/Usuario.java
+++ src/main/java/com/cibertec/api_reservas_mesas/model/Usuario.java
@@ -1,15 +1,19 @@
 package com.cibertec.api_reservas_mesas.model;
 
 import java.util.List;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 
 @Entity
@@ -24,9 +28,15 @@ public class Usuario {
 	private String telefono;
 	private String dni;
 	private boolean estado;
-	@Enumerated(EnumType.STRING)
-	private Rol rol;
 
+	@ManyToMany(fetch = FetchType.EAGER, targetEntity = Rol.class, cascade = CascadeType.PERSIST)
+	@JoinTable(
+			name = "usario_roles", 
+			joinColumns = @JoinColumn(name = "usuario_id"),
+			inverseJoinColumns = @JoinColumn(name = "rol_id")
+	)
+	private Set<Rol> roles;
+	
 	@OneToMany(mappedBy = "cliente")
 	@JsonIgnore
 	private List<Reserva> reservasCliente;
@@ -103,12 +113,12 @@ public class Usuario {
 		this.estado = estado;
 	}
 
-	public Rol getRol() {
-		return rol;
+	public Set<Rol> getRoles() {
+		return roles;
 	}
 
-	public void setRol(Rol rol) {
-		this.rol = rol;
+	public void setRoles(Set<Rol> roles) {
+		this.roles = roles;
 	}
 
 	public List<Reserva> getReservasCliente() {
